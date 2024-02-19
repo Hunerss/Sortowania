b292eeafb9b47668c618dee299d586ecd4a92096
11344 <11344@16S235>
2024-02-19 15:05:43 +0100

    first commit

---
+++ windows/Main.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sortowania.windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Navigate(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Console.WriteLine(btn.Name);
+        }
+    }
+}
---
+++ windows/pages/Practice.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sortowania.windows.pages
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Practice.xaml
+    /// </summary>
+    public partial class Practice : UserControl
+    {
+        public Practice()
+        {
+            InitializeComponent();
+        }
+
+        private void Navigate(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Console.WriteLine(btn.Name);
+        }
+    }
+}
